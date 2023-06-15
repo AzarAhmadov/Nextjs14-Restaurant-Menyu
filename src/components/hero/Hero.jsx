1aa19c6e371d66c0546e08564981956462b72570
Azar Ahmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-06-15 15:24:34 +0400

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,24 +1,23 @@
-
 import { GraphQLClient, gql } from 'graphql-request';
 
 const graphcms = new GraphQLClient('https://api-us-west-2.hygraph.com/v2/cliw4abwt0pza01uh0kc9913j/master');
 
 const QUERY = gql`
 {
-    restaurants {
+    restaurans {
         id
         name
-      image{
-        url
-      }
+        image{
+          url
+        }
       }
 }
 `;
 
 const getDataRestoran = async () => {
     try {
-        const restaurants = await graphcms.request(QUERY);
-        return restaurants;
+        const restaurant = await graphcms.request(QUERY);
+        return restaurant;
     } catch (error) {
         console.error('Error fetching Restoran:', error);
         return [];
@@ -28,22 +27,16 @@ const getDataRestoran = async () => {
 
 const Hero = async () => {
     const data = await getDataRestoran();
-
     return (
         <section id='hero'>
-            {data.restaurants[0] && data.restaurants[0].image && (
-                <section id='hero'>
-                    <img src={data.restaurants[0].image.url} alt="Restaurant-bg" />
-                    <div className="hero-item">
-                        <h1 className="item-text">
-                            {data.restaurants[0].name && (
-                                <div>{data.restaurants[0].name}</div>
-                            )}
-                        </h1>
-                    </div>
-                </section>
-            )}
-
+            <section id='hero'>
+                <img src={data.restaurans[0].image?.url} alt="Restaurant-bg" />
+                <div className="hero-item">
+                    <h1 className="item-text">
+                        {data.restaurans[0].name}
+                    </h1>
+                </div>
+            </section>
         </section>
     );
 };
